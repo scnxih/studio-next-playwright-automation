5a25c1b728f4fb9f9ac695dd2700db1e2f93fd83
Jacky(Jiaqi) Wang <jiaqi.wang@sas.com>
2024-04-03 10:27:06 +0800

    April 1st 2024: 1.Added screenshots for 'deployed schedualed job' dialog 2.Added screenshots for flown detail pane and automatically provide picture name by using inspect

--- src/Pages/StudioNext/Center/deployed_scheduled_job_page.py
+++ src/Pages/StudioNext/Center/deployed_scheduled_job_page.py
@@ -4,6 +4,8 @@ Date: November 02, 2023
 Description: DeployedScheduledJobPage will inherit from CenterPage class。
 
 """
+import time
+
 from src.Pages.StudioNext.Center.center_page import *
 
 
@@ -15,15 +17,22 @@ class DeployedScheduledJobPage(CenterPage):
 
     def run_now(self):
         self.toolbar.click_btn_by_test_id("scheduledJobsPane-runNowButton")
+        self.screenshot(self.base_xpath, "run_schedule")
 
     def edit_schedule(self):
         self.toolbar.click_btn_by_title(Helper.data_locale.EDIT_SCHEDULE)
+        time.sleep(1)
+        self.screenshot(self.base_xpath, "edit_schedule")
 
     def remove_schedule(self):
         self.toolbar.click_btn_by_title(Helper.data_locale.REMOVE_SCHEDULE)
+        time.sleep(1)
+        self.screenshot(self.base_xpath, "remove_schedule")
 
     def refresh_list(self):
         self.toolbar.click_btn_by_title(Helper.data_locale.REFRESH_LIST)
+        time.sleep(1)
+        self.screenshot(self.base_xpath, "refresh_schedule")
 
     """This method has not been implemented yet, need to rewrite in future"""
     def column_setting(self):
