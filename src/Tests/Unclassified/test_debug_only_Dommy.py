fd12b560aa4be5653689c74bfdce9bd60ab5d6b9
Dommy(Fuying) Chen <fuying.chen@sas.com>
2024-09-11 15:20:49 +1000

    Updated by Dommy on Aug 30, 2024 1. Update test_debug_only_Dommy.py and cluster_variables_pane.py file. 2. Add new file text_map_pane.py file.

--- src/Tests/Unclassified/test_debug_only_Dommy.py
+++ src/Tests/Unclassified/test_debug_only_Dommy.py
@@ -478,3 +478,76 @@ def test_13_cluster_variables_in_flow_l1(page, init):
     cluster_variables_pane.set_check_maximum_number_of_iterations()
     cluster_variables_pane.set_iterations("5")
     flow.run(True)
+
+
+def test_14_cluster_variables_in_flow_l1(page, init):
+    flow: FlowPage = PageHelper.new_flow(page)
+    step_path = [Helper.data_locale.STEP_CATEGORY_DEVELOP, Helper.data_locale.STEP_SAS_PROGRAM]
+    flow.add_step_from_stepspane_to_flow(step_path)
+    flow.select_node_in_flow_canvas(Helper.data_locale.SAS_PROGRAM)
+    sas_program_pane = SASProgramPane(page)
+    code = """ 
+        libname AUTOLIB '/segatest/I18N/Autolib' ;    
+        """
+    sas_program_pane.type_into_text_area(code)
+
+    flow.add_node(FlowNodeType.table)
+    flow.select_node_in_flow_canvas(Helper.data_locale.TABLE)
+    table_pane = TablePane(page)
+    table_pane.set_library("AUTOLIB")
+    table_pane.set_table("BASEBALL'中文测试")
+    time.sleep(0.8)
+    flow.link_two_nodes_in_flow(Helper.data_locale.SAS_PROGRAM, "BASEBALL'中文测试")
+    flow.arrange_nodes()
+    flow.run(True)
+
+    step_path = [Helper.data_locale.STEP_CATEGORY_STATISTICS, Helper.data_locale.STEP_CLUSTER_VARIABLES]
+    flow.add_step_from_stepspane_to_flow(step_path)
+    flow.link_two_nodes_in_flow("BASEBALL'中文测试", Helper.data_locale.STEP_CLUSTER_VARIABLES)
+    flow.arrange_nodes()
+
+    flow.click_context_menu_for_the_node_in_flow(Helper.data_locale.STEP_CLUSTER_VARIABLES, "添加输出端口",
+                                                 "{sasstudio-steps-gui-icu.clustervariables.outputports"
+                                                 ".statDSName.displayname.title}")
+    flow.add_node(FlowNodeType.table)
+    flow.select_node_in_flow_canvas(Helper.data_locale.TABLE)
+    table_pane.set_library("work")
+    table_pane.set_table("输出'stat")
+    flow.link_two_nodes_in_flow(Helper.data_locale.STEP_CLUSTER_VARIABLES,"输出'stat")
+    flow.arrange_nodes()
+
+    flow.click_context_menu_for_the_node_in_flow(Helper.data_locale.STEP_CLUSTER_VARIABLES, "添加输出端口",
+                                                 "{sasstudio-steps-gui-icu.clustervariables.outputports"
+                                                 ".treeDSName.displayname.title}")
+    flow.add_node(FlowNodeType.table)
+    flow.select_node_in_flow_canvas(Helper.data_locale.TABLE)
+    table_pane.set_library("work")
+    table_pane.set_table("输出'tree")
+    flow.link_two_nodes_in_flow(Helper.data_locale.STEP_CLUSTER_VARIABLES, "输出'tree")
+    flow.arrange_nodes()
+
+    flow.select_node_in_flow_canvas(Helper.data_locale.STEP_CLUSTER_VARIABLES)
+    cluster_variables_pane = ClusterVariablesPane(page)
+    cluster_variables_pane.set_filter_input_data("UPPER('Division''中'n) = '东部'")
+    cluster_variables_pane.add_columns_for_variables_to_cluster(check_column_name_list=["nAtBat'中", "nHits'中"])
+    cluster_variables_pane.expand_windowshade_additional_roles()
+    cluster_variables_pane.add_columns_for_variables_to_partial_out(check_column_name_list=["nHome'中", "nRuns'中"])
+    cluster_variables_pane.add_column_for_frequency_count("nRBI'中")
+    cluster_variables_pane.add_column_for_weight("YrMajor'中")
+    cluster_variables_pane.add_columns_for_group_analysis_by(check_column_name_list=["Team'中文"])
+
+    cluster_variables_pane.click_options_tab()
+    cluster_variables_pane.set_method(item_index=1)
+    cluster_variables_pane.set_check_minimum_proportion_of_variation()
+    cluster_variables_pane.set_proportion("0.2")
+    cluster_variables_pane.expand_windowshade_deatails()
+    cluster_variables_pane.set_analyze(item_index=1)
+    cluster_variables_pane.set_check_maximum_number_of_iterations()
+    cluster_variables_pane.set_iterations("5")
+
+    cluster_variables_pane.click_output_tab()
+    cluster_variables_pane.set_check_create_statistics_data()
+    #cluster_variables_pane.set_check_replace_existing_output_table_for_statistics()
+    cluster_variables_pane.set_check_create_tree_information_data()
+    #cluster_variables_pane.set_check_replace_existing_output_table_for_tree_information()
+    flow.run(True)
