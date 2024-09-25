a741c0fa3dd9eb9a15c019e2fcc907a395bb4f49
Alice <xin.hao@sas.com>
2024-09-25 15:59:24 +0800

    Updated by Alice on 2024/09/25: 1. Add screenshots in test_samebirpro_01.py.

--- src/Tests/Flow/TaskToStep/Statistics/test_samebirpro_01.py
+++ src/Tests/Flow/TaskToStep/Statistics/test_samebirpro_01.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 
 from src.Helper.page_helper import PageHelper
@@ -19,7 +21,7 @@ Date: September 11th, 2024
 def test_init(page, init):
     PageHelper.init_environments(page)
 
-
+@pytest.mark.level0_step
 def test_01_same_birthday_lev0(page, init):
     """
     Level-0 testcase for Statistics/Same Birthday Probability
@@ -34,28 +36,37 @@ def test_01_same_birthday_lev0(page, init):
     flow.add_step_from_stepspane_to_flow(step_path)
 
     # Connect output port with same birthday node
-    flow.apply_detail_layout_vertical()
+    # flow.apply_detail_layout_vertical()
+    flow.apply_flow_layout_vertical()
     flow.select_node_in_flow_canvas(Helper.data_locale.STEP_SAME_BIRTHDAY_PROBABILITY)
 
     same_birthday_probability_pane = SameBirthdayProbabilityPane(page)
     same_birthday_probability_pane.set_number_of_people_in_a_room("145")
+    same_birthday_probability_pane.screenshot_self("options")
 
     flow.run(True)
-
-    flow.tab_group.click_tab_by_text(Helper.data_locale.SUBMITTED_CODE_AND_RESULTS)
+    time.sleep(1)
+    flow.screenshot_self("run")
+    # flow.tab_group.click_tab_by_text(Helper.data_locale.SUBMITTED_CODE_AND_RESULTS)
+    flow.click_log_tab()
+    time.sleep(0.5)
+    flow.screenshot_self("log")
     # flow.tab_group.click_tab_by_text(Helper.data_locale.OUTPUT_DATA + "(1)")
-    flow.tab_group.click_tab_by_text("输出数据 (1)")
-    time.sleep(3)
+    # flow.tab_group.click_tab_by_text("输出数据 (1)")
+    flow.click_output_data_tab()
+    time.sleep(0.5)
+    flow.screenshot_self("output_data")
+    # time.sleep(3)
 
     # flow.screenshot_self(pic_name="flow_results_same_birthday")
     # Mask table name
     # //h5[@data-testid="dataPane-toolbar-nameHeading"]
-    flow.screenshot_self(pic_name="flow_results_same_birthday",
-                         mask=['//h5[@data-testid="dataPane-toolbar-nameHeading"]'],
-                         mask_color="#000000")
+    # flow.screenshot_self(pic_name="flow_results_same_birthday",
+    #                      mask=['//h5[@data-testid="dataPane-toolbar-nameHeading"]'],
+    #                      mask_color="#000000")
     # WholePage(page).screenshot_self(pic_name="flow_results_same_birthday")
 
-
+@pytest.mark.level1_step
 def test_02_same_birthday_lev1(page, init):
     """
     Level-1 testcase for Statistics/Same Birthday Probability
@@ -82,23 +93,30 @@ def test_02_same_birthday_lev1(page, init):
     flow.select_output_port_node_in_flow(Helper.data_locale.STEP_SAME_BIRTHDAY_PROBABILITY)
     flow.link_two_nodes_in_flow(Helper.data_locale.STEP_SAME_BIRTHDAY_PROBABILITY, "prob_same_birthday")
     flow.arrange_nodes()
-    flow.apply_detail_layout_vertical()
+    flow.apply_flow_layout_vertical()
     flow.select_node_in_flow_canvas(Helper.data_locale.STEP_SAME_BIRTHDAY_PROBABILITY)
 
     same_birthday_probability_pane = SameBirthdayProbabilityPane(page)
     same_birthday_probability_pane.set_number_of_people_in_a_room("2")
 
     flow.run(True)
-
-    flow.tab_group.click_tab_by_text(Helper.data_locale.SUBMITTED_CODE_AND_RESULTS)
-    # flow.tab_group.click_tab_by_text(Helper.data_locale.OUTPUT_DATA + "(1)")
-    flow.tab_group.click_tab_by_text("输出数据 (1)")
     time.sleep(3)
+    flow.screenshot_self("run")
+    flow.click_log_tab()
+    time.sleep(0.5)
+    flow.screenshot_self("log")
+    flow.click_output_data_tab()
+    time.sleep(0.5)
+    flow.screenshot_self("output_data")
+    # flow.tab_group.click_tab_by_text(Helper.data_locale.SUBMITTED_CODE_AND_RESULTS)
+    # flow.tab_group.click_tab_by_text(Helper.data_locale.OUTPUT_DATA + "(1)")
+    # flow.tab_group.click_tab_by_text("输出数据 (1)")
+    # time.sleep(3)
 
     # flow.screenshot_self(pic_name="flow_results_same_birthday")
     # Mask table name
     # //h5[@data-testid="dataPane-toolbar-nameHeading"]
-    flow.screenshot_self(pic_name="flow_results_same_birthday",
-                         mask=['//h5[@data-testid="dataPane-toolbar-nameHeading"]'],
-                         mask_color="#000000")
+    # flow.screenshot_self(pic_name="flow_results_same_birthday",
+    #                      mask=['//h5[@data-testid="dataPane-toolbar-nameHeading"]'],
+    #                      mask_color="#000000")
     # WholePage(page).screenshot_self(pic_name="flow_results_same_birthday")
