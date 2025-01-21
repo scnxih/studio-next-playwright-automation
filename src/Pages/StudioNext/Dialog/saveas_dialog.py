d68ee90e7be2ddb6862eb9b4e2c883e5b4832831
Jacky(Jiaqi) Wang <jiaqi.wang@sas.com>
2025-01-21 10:42:54 +0800

    Tuesday, January 21, 2025 #1
    # LOCATION
    src.Pages.StudioNext.Dialog.saveas_dialog.SaveAsDialog.selfie
    
    # DESCRIPTION
    Comment out masks previously used in {Save As} dialog.

--- src/Pages/StudioNext/Dialog/saveas_dialog.py
+++ src/Pages/StudioNext/Dialog/saveas_dialog.py
@@ -72,7 +72,7 @@ class SaveAsDialog(Dialog):
         """
         Helper.logger.debug("SaveAsDialog: Overwrite the vanilla screenshot_self method in BasePage")
         self.screenshot(self.base_xpath, pic_name, clip=clip,
-                        mask=[self.bread_crumb, self.content_selector_navigator_tree, self.temp_content_selector],
+                        # mask=[self.bread_crumb, self.content_selector_navigator_tree, self.temp_content_selector],  # Comment out masks Jan 21, 2025
                         mask_color="#000000")
 
     def wait_for_open(self):
