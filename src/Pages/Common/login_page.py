282be4eb7f9ddbaf9a135282c45345326468e66e
Jacky(Jiaqi) Wang <jiaqi.wang@sas.com>
2025-04-06 13:00:21 +0800

    Sunday, April 6, 2025, #1
    # LOCATION
    src.Pages.Common.login_page.LoginPage._login
    
    # DESCRIPTION
    Use mwf server, for both daily & 3xdaily are unavailable with compute context refreshing interminably.

--- src/Pages/Common/login_page.py
+++ src/Pages/Common/login_page.py
@@ -26,11 +26,11 @@ class LoginPage(BasePage):
 
     def _login(self):
         # Original
-        url = "https://3xdaily.pgc.unx.sas.com/SASStudioNext/"
+        # url = "https://3xdaily.pgc.unx.sas.com/SASStudioNext/"
 
         # Backup server
         # url = "https://daily.pgc.unx.sas.com/SASStudioNext/"
-        # url = "https://mwf.pgc.unx.sas.com/SASStudioNext/"
+        url = "https://mwf.pgc.unx.sas.com/SASStudioNext/"
         # url = "https://tth.pgc.unx.sas.com/SASStudioNext/"
         # url = "https://analyst.dev.pgc.unx.sas.com/SASStudioNext/"
         # url = "https://analyst.de.dev.unx.sas.com/SASStudioNext/"
