f4d08f43f9545561641e0d4a32af049a248ba93d
Jacky(Jiaqi) Wang <jiaqi.wang@sas.com>
2025-04-15 09:02:45 +0800

    Tuesday, April 15, 2025, #1
    # LOCATION
    src.Pages.Common.login_page.LoginPage._login
    
    # DESCRIPTION
    /SASStudio will now be the react version, reflecting the prosperous ship in 2025.06.

--- src/Pages/Common/login_page.py
+++ src/Pages/Common/login_page.py
@@ -26,10 +26,10 @@ class LoginPage(BasePage):
 
     def _login(self):
         # Original
-        # url = "https://3xdaily.pgc.unx.sas.com/SASStudioNext/"
+        url = "https://3xdaily.pgc.unx.sas.com/SASStudio/"
 
         # Backup server
-        url = "https://daily.pgc.unx.sas.com/SASStudioNext/"
+        # url = "https://daily.pgc.unx.sas.com/SASStudioNext/"
         # url = "https://mwf.pgc.unx.sas.com/SASStudioNext/"
         # url = "https://tth.pgc.unx.sas.com/SASStudioNext/"
         # url = "https://analyst.dev.pgc.unx.sas.com/SASStudioNext/"
