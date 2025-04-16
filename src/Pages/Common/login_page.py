338ec1a3d01de66ef5ba1f12854b3f9c9c6426b8
Jacky(Jiaqi) Wang <jiaqi.wang@sas.com>
2025-04-16 18:01:29 +0800

    Wednesday, April 16, 2025, #2
    # LOCATION
    src.Pages.Common.login_page.LoginPage._login
    
    # DESCRIPTION
    Reverted to 3xdaily server, hoping its availability on Thursday, April 17, 2025.

--- src/Pages/Common/login_page.py
+++ src/Pages/Common/login_page.py
@@ -26,11 +26,11 @@ class LoginPage(BasePage):
 
     def _login(self):
         # Original
-        # url = "https://3xdaily.pgc.unx.sas.com/SASStudio/"
+        url = "https://3xdaily.pgc.unx.sas.com/SASStudio/"
 
         # Backup server
         # url = "https://daily.pgc.unx.sas.com/SASStudio/"
-        url = "https://mwf.pgc.unx.sas.com/SASStudio/"
+        # url = "https://mwf.pgc.unx.sas.com/SASStudio/"
         # url = "https://tth.pgc.unx.sas.com/SASStudio/"
         # url = "https://analyst.dev.pgc.unx.sas.com/SASStudioNext/"
         # url = "https://analyst.de.dev.unx.sas.com/SASStudio/"
