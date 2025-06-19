1dbcfdc05b616b215b3db1bdb0ff8cc6c51296e6
Percy Liu <jia.liu@sas.com>
2025-06-19 05:45:52 -0400

    Modify binning test case

--- src/Tests/Flow/TaskToStep/PrepareAndExploreData/test_binning_01.py
+++ src/Tests/Flow/TaskToStep/PrepareAndExploreData/test_binning_01.py
@@ -61,7 +61,16 @@ run;
     step_path = [Helper.data_locale.STEP_CATEGORY_PREPARE_AND_EXPLORE_DATA, Helper.data_locale.STEP_Binning]
     flow.add_step_from_stepspane_to_flow(step_path)
     flow.link_two_nodes_in_flow("BASEBALL'中文测试", Helper.data_locale.STEP_Binning)
+    flow.arrange_nodes()
+
+    flow.click_add_output_port_in_context_menu_on_node(Helper.data_locale.STEP_Binning)
+    flow.add_node(FlowNodeType.table)
+    flow.select_node_in_flow_canvas(Helper.data_locale.TABLE)
+    table_pane.set_library("CASUSER")
+    table_pane.set_table("OUT'中文")
+    flow.link_two_nodes_in_flow(Helper.data_locale.STEP_Binning, "OUT'中文")
     flow.click_on_canvas_in_flow()
+    flow.arrange_nodes()
 
     flow.select_node_in_flow_canvas(Helper.data_locale.STEP_Binning)
     BinningPane_pane =BinningPane(page)
@@ -71,5 +80,14 @@ run;
 
     BinningPane_pane.click_options_tab()
     BinningPane_pane.set_number_of_bins("10")
+    BinningPane_pane.set_method(1)
+
+    BinningPane_pane.click_output_tab()
+    BinningPane_pane.set_check_save_binned_data()
+    BinningPane_pane.set_include_variables_from_input(1)
+
+    BinningPane_pane.set_check_save_Scoring_code()
+    BinningPane_pane.set_specify_data_to_show(1)
+    BinningPane_pane.set_number_of_observations_increment(3)
 
     flow.run(True)
